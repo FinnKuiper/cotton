ae62a579b7d5a9653e8cebb29e6da47e4337b6bd
Finn Kuiper <finnkuiper05@gmail.com>
2023-12-19 23:34:35 +0100

    Added database and prefix system

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
+# imports
 import discord
+import os
 from discord.ext import commands
 from dotenv import load_dotenv
-import os
+from pymongo_get_database import get_database
 
+# load environment variables
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 
+# create bot
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix='!', intents=intents)
 
+# create bot and get the command prefix from the database
+async def get_prefix(bot, message):
+    # get database
+    dbname = get_database()
+    collection_name = dbname["guild"]
+    
+    # get prefix
+    guild_id = message.guild.id
+    guild = collection_name.find_one({"guild_id": guild_id})
+    prefix = guild["prefix"] if guild else "!"
+    return prefix
+
+bot = commands.Bot(command_prefix=get_prefix, intents=intents)
+
+# load cogs
 @bot.event
 async def on_ready():
     for filename in os.listdir('./cogs'):
@@ -19,4 +37,26 @@ async def on_ready():
             print(f'{command} loaded')
     print('Bot is ready')
 
+# on added to guild add to mongo database
+@bot.event
+async def on_guild_join(guild):
+    # get database
+    dbname = get_database()
+    collection_name = dbname["guild"]
+    
+    # create item
+    item = {
+        "name": guild.name,
+        "guild_id": guild.id,
+        "owner": guild.owner,
+        "update_channel": None,
+        "prefix": "!",
+        "expiry_date": None
+    }
+    
+    # insert item
+    collection_name.insert_one(item)
+    print(f'Added {guild.name} to database')
+
+# run bot
 bot.run(token)
