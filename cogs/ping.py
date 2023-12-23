0b9d01f1ed7e0534cdcea3c2f4095a6100aece4e
Finn Kuiper <finnkuiper05@gmail.com>
2023-12-23 15:49:34 +0100

    Refactored to slash command

--- cogs/ping.py
+++ cogs/ping.py
@@ -1,16 +1,24 @@
 from discord.ext import commands
+from discord import app_commands
+from dotenv import load_dotenv
+import os
+import discord
+
+load_dotenv()
+guild_id = int(os.getenv("GUILD_ID"))
+
 
 class Ping(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
+    @commands.hybrid_command(
+        name="ping", with_app_command=True, description="Get the ping of the bot"
+    )
+    @app_commands.guilds(discord.Object(id=guild_id))
     async def ping(self, ctx):
-        await ctx.send('Pong!')
-    
-    @commands.Cog.listener()
-    async def on_ready(self):
-        print('Ping is ready')
+        await ctx.send("Pong!")
+
 
 async def setup(bot):
     await bot.add_cog(Ping(bot))
