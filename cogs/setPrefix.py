da28576b1d1def6113621e9cffe593f605512b40
Finn Kuiper <finnkuiper05@gmail.com>
2023-12-23 15:49:47 +0100

    Refactored to slash command

--- cogs/setPrefix.py
+++ cogs/setPrefix.py
@@ -1,17 +1,26 @@
+import discord
 from discord.ext import commands
+from discord import app_commands
+from dotenv import load_dotenv
+import os
 from pymongo_get_database import get_database
+
+load_dotenv()
+guild_id = int(os.getenv("GUILD_ID"))
+
 
 class SetPrefix(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def setprefix(self, ctx, arg1=None):
-        # make sure the user is an admin
-        if not ctx.author.guild_permissions.administrator:
-            await ctx.send("You don't have the permission to use this command")
-            return
-        
+    @commands.hybrid_command(
+        name="setprefix",
+        with_app_command=True,
+        description="Set the prefix for the bot",
+    )
+    @app_commands.guilds(discord.Object(id=guild_id))
+    @commands.has_permissions(administrator=True)
+    async def setprefix(self, ctx: commands.Context, prefix: str = None):
         guild = ctx.guild
         # get database
         dbname = get_database()
@@ -20,29 +29,32 @@ class SetPrefix(commands.Cog):
         # check if guild is already in the system
         guild_data = collection_name.find_one({"guild_id": guild.id})
         if not guild_data:
-            await ctx.send(f"{guild.name} is not in the setted up yet! Use the command `!setup` to add the guild to the system")
+            await ctx.send(
+                f"{guild.name} is not in the setted up yet! Use the command `!setup` to add the guild to the system"
+            )
             return
-        
-        # check if arg1 is valid
-        if len(arg1) > 1:
-            await ctx.send('The prefix can only be one character long')
-            return
-        
+
         # get prefix
-        prefix = guild_data["prefix"]
-        
-        if arg1 == None:
-            await ctx.send(f'The current prefix is `{prefix}`')
+        oldPrefix = guild_data["prefix"]
+
+        if prefix == None:
+            await ctx.send(f"The current prefix is `{oldPrefix}`")
             return
         else:
+            if len(prefix) > 1:
+                await ctx.send("The prefix can only be one character long")
+                return
             # update prefix
-            collection_name.update_one({"guild_id": guild.id}, {"$set": {"prefix": arg1}})
-            await ctx.send(f'Prefix changed to `{arg1}`')
+            collection_name.update_one(
+                {"guild_id": guild.id}, {"$set": {"prefix": prefix}}
+            )
+            await ctx.send(f"Prefix changed to `{prefix}`")
             return
-    
+
     @commands.Cog.listener()
     async def on_ready(self):
-        print('Setup is ready')
+        print("Setup is ready")
+
 
 async def setup(bot):
     await bot.add_cog(SetPrefix(bot))
