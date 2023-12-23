a89b00c47132f99e01850afda167f41f7b2719f8
Finn Kuiper <finnkuiper05@gmail.com>
2023-12-23 15:50:38 +0100

    Refactored to slash command

--- cogs/setUpdateChannel.py
+++ cogs/setUpdateChannel.py
@@ -1,16 +1,23 @@
 import discord
 from discord.ext import commands
+from discord import app_commands
+from dotenv import load_dotenv
+import os
 from pymongo_get_database import get_database
+
+load_dotenv()
+guild_id = int(os.getenv("GUILD_ID"))
 
 
 class SetUpdateChannel(commands.Cog):
-    @commands.command()
-    async def setupdatechannel(self, ctx, arg1=None):
-        # make sure the user is an admin
-        if not ctx.author.guild_permissions.administrator:
-            await ctx.send("You don't have the permission to use this command")
-            return
-        
+    @commands.hybrid_command(
+        name="setupdatechannel",
+        with_app_command=True,
+        description="Set the update channel for the bot",
+    )
+    @app_commands.guilds(discord.Object(id=guild_id))
+    @commands.has_permissions(administrator=True)
+    async def setupdatechannel(self, ctx, channel: discord.TextChannel):
         guild = ctx.guild
         # get database
         dbname = get_database()
@@ -19,31 +26,36 @@ class SetUpdateChannel(commands.Cog):
         # check if guild is already in the system
         guild_data = collection_name.find_one({"guild_id": guild.id})
         if not guild_data:
-            await ctx.send(f"{guild.name} is not in the setted up yet! Use the command `!setup` to add the guild to the system")
+            await ctx.send(
+                f"{guild.name} is not in the setted up yet! Use the command `!setup` to add the guild to the system"
+            )
             return
-        
+
         # get current update channel
         update_channel_id = guild_data["update_channel"]
         update_channel = guild.get_channel(update_channel_id)
-        
-        if arg1 == None or not arg1.isdigit():
+
+        if channel == None or not channel.isdigit():
             if update_channel or None:
-                await ctx.send(f'The current update channel is {update_channel.mention}')
+                await ctx.send(
+                    f"The current update channel is {update_channel.mention}"
+                )
                 return
             else:
-                await ctx.send('No update channel has been set yet')
+                await ctx.send("No update channel has been set yet")
                 return
         else:
             # update update channel
-            collection_name.update_one({"guild_id": guild.id}, {"$set": {"update_channel": int(arg1)}})
-            await ctx.send(f'Prefix changed to `{update_channel.mention}`')
+            collection_name.update_one(
+                {"guild_id": guild.id}, {"$set": {"update_channel": int(channel)}}
+            )
+            await ctx.send(f"Prefix changed to `{update_channel.mention}`")
             return
-    
+
     @commands.Cog.listener()
     async def on_ready(self):
-        print('Setup is ready')
+        print("Setup is ready")
 
-    
 
 async def setup(bot):
     await bot.add_cog(SetUpdateChannel(bot))
