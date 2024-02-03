f411fb2fa0782c5baea95e0b36f90c8afe94e4e1
FinnKuiper <finnkuiper05@gmail.com>
2024-02-03 17:18:38 +0100

    Add Hug cog with hug command

---
+++ cogs/hug.py
@@ -0,0 +1,30 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+guild_id = int(os.getenv("GUILD_ID"))
+
+
+class Hug(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.hybrid_command(
+        name="hug",
+        with_app_command=True,
+        description="Hug some... or yourself?",
+        aliases=["h"],
+    )
+    @app_commands.guilds(discord.Object(id=guild_id))
+    async def hug(self, ctx, user: discord.Member = None):
+        if user:
+            await ctx.send(f"{ctx.author.mention} hugged {user.mention}")
+        else:
+            await ctx.send(f"{ctx.author.mention} hugged themselves!")
+
+
+async def setup(bot):
+    await bot.add_cog(Hug(bot))
